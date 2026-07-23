ce3b9d92c006e4ff167c20572134f7a9da154901
GritsH <grits.lena@gmail.com>
2026-07-23 13:54:04 +0300

    add keycloak

--- src/main/java/com/grits/userservice/config/SecurityConfiguration.java
+++ src/main/java/com/grits/userservice/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.grits.userservice.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,11 +19,9 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/v1/**"
-                        ).permitAll()
                         .anyRequest().authenticated()
                 )
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
                 .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for testing
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
